c2f80c0a6c1aee70973e40019695e61037af06d4
Tor Rivera <tor@trivera.net>
2025-12-19 17:27:03 +0100

    feat: Add iOS PWA support with apple-touch-icon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="theme-color" content="#FF793F">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <link rel="manifest" href="/build/manifest.webmanifest">
+    <link rel="apple-touch-icon" href="/icons/apple-touch-icon-180x180.png">
 
     <title>{{ $title ?? config('app.name') }}</title>
 
